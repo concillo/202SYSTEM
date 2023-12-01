06435028e79ed44221c755d9553e1495d24338f7
Concillo <151208783+concillo@users.noreply.github.com>
2023-12-02 03:08:29 +0800

    Revert "Serot"

--- admin/issue_requests.php
+++ admin/issue_requests.php
@@ -21,7 +21,7 @@ if ($_SESSION['RollNo']) {
    
 
    body {font-family: 'Roboto', sans-serif;
-        background: url('images/colors.jpg') no-repeat;
+        background: url('images/issue.png') no-repeat;
         background-size: cover;
         background-position: center;
        padding:50px;
