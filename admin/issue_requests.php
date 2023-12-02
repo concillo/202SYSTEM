aafe0c474ed5a17d55529ec334cd0598405313f7
abogadomicaaa <152603924+abogadomicaaa@users.noreply.github.com>
2023-12-02 08:22:59 +0800

    [serot] requests background

--- admin/issue_requests.php
+++ admin/issue_requests.php
@@ -22,7 +22,7 @@ if ($_SESSION['RollNo']) {
 
    body {font-family: 'Roboto', sans-serif;
 
-        background: url('../images/colors.jpg') no-repeat;
+        background: url('images/purple.jpg') no-repeat;
 
         background-size: cover;
         background-position: center;
